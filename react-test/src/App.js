dd97aa901e84f8b50cb332ba3bc5e9c50c831492
wangkaiwd <1162486176@qq.com>
2020-02-16 18:17:40 +0800

    feat: jest-enzyme config

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // 可以使用data-test自定义属性来标记dom属性，通过enzyme来的api来进行选择
+    <div className="App" title="test title" data-test="app">
+      hello world
     </div>
   );
 }
