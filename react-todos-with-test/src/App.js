1b307ae2f76f1d0741ac6cd7a2449037504c26c0
wangkaiwd <1162486176@qq.com>
2020-02-16 20:49:55 +0800

    feat: react test code supply

---
+++ react-todos-with-test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoList from './container/todoList/TodoList';
+
+function App () {
+  return (
+    <div className="App" data-test="app">
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
