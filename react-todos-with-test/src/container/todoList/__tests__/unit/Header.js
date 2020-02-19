5e722a2e9a5bf82d01fd352c378984fc84220fb3
wangkaiwd <1162486176@qq.com>
2020-02-19 21:59:07 +0800

    feat: 全部测试用例通过，并找变量不能缓存的相关介绍

--- react-todos-with-test/src/container/todoList/__tests__/unit/Header.js
+++ react-todos-with-test/src/container/todoList/__tests__/unit/Header.js
@@ -25,4 +25,29 @@ describe('Header 组件', () => {
     // 这里为什么需要重新获取input?
     expect(container.find('input').prop('value')).toBe(inputValue);
   });
+  it('按下回车后，input框无内容，无操作', () => {
+    const fn = jest.fn();
+    const container = shallow(<Header addItem={fn}/>);
+    container.find('input').simulate('change', {
+      target: { value: '' }
+    });
+    container.find('input').simulate('keyup', {
+      target: { keyCode: 13 }
+    });
+    expect(fn).not.toHaveBeenCalled();
+  });
+  it('按下回车后，input框有内容，函数被调用', () => {
+    const fn = jest.fn();
+    const container = shallow(<Header addItem={fn}/>);
+    // 每次都要重新获取才行: https://github.com/airbnb/enzyme/issues/2139
+    // 官方文档： https://github.com/airbnb/enzyme/blob/master/docs/guides/migration-from-2-to-3.md#calling-props-after-a-state-change
+    // 官方文档演示例子： https://github.com/airbnb/enzyme/blob/master/docs/guides/migration-from-2-to-3.md#calling-props-after-a-state-change
+    container.find('input').simulate('change', {
+      target: { value: 'something' }
+    });
+    container.find('input').simulate('keyup', {
+      target: { keyCode: 13 }
+    });
+    expect(fn).toHaveBeenCalled();
+  });
 });
--- react-todos-with-test/src/container/todoList/components/Header.jsx
+++ react-todos-with-test/src/container/todoList/components/Header.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 
-const Header = () => {
+const Header = (props) => {
   const [value, setValue] = useState('');
   const onChange = e => setValue(e.target.value);
-
+  const onKeyUp = (e) => {
+    if (value === '') {return;}
+    if (e.target.keyCode === 13) {
+      props.addItem(value);
+    }
+  };
   return (
     <div>
-      <input value={value} type="text" onChange={onChange} data-test="input"/>
+      <input value={value} type="text" onKeyUp={onKeyUp} onChange={onChange} data-test="input"/>
     </div>
   );
 };
