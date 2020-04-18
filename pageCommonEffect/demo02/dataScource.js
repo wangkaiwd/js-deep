08f5d9386e10f6da6f2f8c0adcb41f38a4e96db1
wangkaiwd <1162486176@qq.com>
2020-04-18 23:04:34 +0800

    feat: complete static style of z tree

---
+++ pageCommonEffect/demo02/dataScource.js
@@ -0,0 +1,18 @@
+const dataSource = [
+  {
+    name: 'test1',
+    open: true,
+    children: [
+      { name: 'test1_1' },
+      { name: 'test1_2' },
+    ],
+  },
+  {
+    name: 'test2',
+    open: true,
+    children: [
+      { name: 'test2_1' },
+      { name: 'test2_2' },
+    ],
+  },
+];
