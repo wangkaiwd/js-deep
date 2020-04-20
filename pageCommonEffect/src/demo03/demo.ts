6b4a3b2cdc0701016026445758a0b88356d4c8e5
wangkaiwd <1162486176@qq.com>
2020-04-20 22:05:27 +0800

    feat: implement drag and drop modal

--- pageCommonEffect/src/demo03/demo.ts
+++ pageCommonEffect/src/demo03/demo.ts
@@ -1,20 +1,40 @@
-const modal = document.querySelector<HTMLDivElement>('.modal')!;
-const boundary = {};
-let isPressMouse = false;
+const selector = '.self-modal';
+const modal = document.querySelector<HTMLDivElement>(selector)!;
+const { width, height } = modal.getBoundingClientRect();
+// 浏览器窗口的宽度和高度(包含滚动条): window.innerHeight,window.innerWidth
+const boundary = {
+  minX: 0,
+  minY: 0,
+  maxX: window.innerWidth - width,
+  maxY: window.innerHeight - height,
+};
+let offsetX = 0, offsetY = 0;
+let pressing = false;
 modal.addEventListener('mousedown', (e: MouseEvent) => {
-  isPressMouse = true;
-});
-modal.addEventListener('mouseup', (e: MouseEvent) => {
-  isPressMouse = false;
-});
-modal.addEventListener('mousemove', (e: MouseEvent) => {
-  if (!isPressMouse) {return;}
+  pressing = true;
   const { clientX, clientY } = e;
-  const currentTarget = e.currentTarget as HTMLDivElement;
-  if (currentTarget) {
-    const { left, top } = currentTarget.getBoundingClientRect();
-    // FIXME:这是在没有出现滚动条的情况
-    currentTarget.style.left = clientX + 'px';
-    currentTarget.style.top = clientY + 'px';
-  }
+  const { top, left } = modal.getBoundingClientRect();
+  offsetX = clientX - left;
+  offsetY = clientY - top;
+
+});
+
+document.addEventListener('mouseup', (e: MouseEvent) => {
+  pressing = false;
+  offsetX = 0;
+  offsetY = 0;
+});
+document.addEventListener('mousemove', (e: MouseEvent) => {
+  if (!pressing) {return;}
+  const { clientX, clientY } = e;
+  // 思路还要梳理一遍,最好画图
+  const { minY, maxY, minX, maxX } = boundary;
+  let moveX = clientX - offsetX;
+  let moveY = clientY - offsetY;
+  if (moveX <= minX) {moveX = minX;}
+  if (moveX >= maxX) {moveX = maxX;}
+  if (moveY <= minY) {moveY = minY;}
+  if (moveY >= maxY) {moveY = maxY;}
+  modal.style.top = moveY + height / 2 + 'px';
+  modal.style.left = moveX + width / 2 + 'px';
 });
---
+++ pageCommonEffect/src/demo03/readme.md
@@ -0,0 +1,8 @@
+## 拖拽模态框
+* 屏幕内拖拽的逻辑
+* 使用事件委托，监听`document`的`mouseup`事件和`mousemove`事件，防止拖拽过快移出模态框的问题
+
+获取浏览器可视窗口的宽高(包括滚动条)：
+* [`window.innerHeight`](https://developer.mozilla.org/zh-CN/docs/Web/API/Window/innerHeight)
+* [`window.innnerWidth`](https://developer.mozilla.org/zh-CN/docs/Web/API/Window/innerWidth)
+
