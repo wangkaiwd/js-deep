83dba0d74f7f615075ed0681f6e59d0ddec78bb6
wangkaiwd <1162486176@qq.com>
2020-04-25 18:33:48 +0800

    feat: design dialog plugin how to use

---
+++ pageCommonEffect/src/demo04/demo.ts
@@ -0,0 +1,15 @@
+class Dialog {
+
+}
+
+// 如何使用dialog
+
+// 结合`jQuery`源码技巧
+
+// const dialog = new Dialog(selector,options)
+// options = {
+//   title: '',
+//   buttons: [],
+//   onCancel,
+//   onOk
+// }
