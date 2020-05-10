c2f6690eec42df88fc667a3e9c0aa374d8910941
wangkaiwd <1162486176@qq.com>
2020-05-11 01:13:24 +0800

    feat: implement promise basic functionality

--- advanced/async/demo04.js
+++ advanced/async/demo04.js
@@ -7,17 +7,47 @@ class Promise {
   constructor (executor) {
     this.status = PENDING;
     this.value = undefined;
-    const resolve = (result) => {
-      this.value = result;
-    };
-    const reject = (reason) => {
-      this.value = reason;
-    };
-    executor(resolve, reject);
+    this.rejectFnList = [];
+    this.resolveFnList = [];
+
+    const resolve = this.resolve.bind(this);
+    const reject = this.reject.bind(this);
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e.message);
+    }
+  }
+
+  resolve (result) {
+    if (this.status !== PENDING) return;
+    this.value = result;
+    this.status = RESOLVED;
+    this.resolveFnList.forEach(resolveFn => {
+      resolveFn(this.value);
+    });
+  }
+
+  reject (reason) {
+    // Promise的state和value一旦确定将不能再次修改
+    if (this.status !== PENDING) return;
+    this.value = reason;
+    this.status = REJECTED;
+    this.rejectFnList.forEach(rejectFn => {
+      rejectFn(this.value);
+    });
   }
 
   then (onFulfilled, onRejected) {
-
+    if (this.status === RESOLVED) {
+      onFulfilled(this.value);
+    } else if (this.status === REJECTED) {
+      onRejected(this.value);
+    } else {
+      this.rejectFnList.push(onRejected);
+      this.resolveFnList.push(onFulfilled);
+    }
   }
 }
 
+module.exports = Promise;
--- advanced/async/test.js
+++ advanced/async/test.js
@@ -1,9 +1,22 @@
 const Promise = require('./demo04');
-
+console.log(1);
 const p1 = new Promise((resolve, reject) => {
-  resolve('hh');
+  console.log(2);
+  // setTimeout(() => {
+  // reject('hh');
+  // 这里的报错捕获不到
+  // throw new Error('exception');
+  // }, 1000);
+  resolve('hello');
+  // throw new Error('exception!');
 });
-p1.then(() => {
-
-});
+p1.then(
+  (result) => {
+    console.log('result', result);
+  },
+  (err) => {
+    console.log('err', err);
+  }
+);
+console.log(4);
 
