102234818c5b73dcc4b913685c314fa0d824a330
wangkaiwd <1162486176@qq.com>
2020-05-21 22:42:02 +0800

    feat: abstract class by new.target check

--- advanced/es6/demo02.js
+++ advanced/es6/demo02.js
@@ -1,6 +1,10 @@
 class Animal {
   constructor (name) {
     this.name = name;
+    // 类的实例化监测
+    if (new.target === Animal) { // 类为抽象类，只能继承，不能进行实例化
+      throw new Error('not new');
+    }
   }
 
   static a () {
@@ -17,6 +21,12 @@ class Tiger extends Animal {
     super(name); // Animal.call(this)
   }
 
+  say2 () {
+    // 原型方法中，super是父类的原型
+    // Animal.prototype
+    super.say();
+  }
+
   static b () { // 静态方法中的super方法指向的是父类
     return super.a();
   }
@@ -28,4 +38,4 @@ tiger.say();
 console.log(tiger.name);
 console.log(Tiger.a()); // 继承静态方法
 console.log(Tiger.b());
-
+tiger.say2();
