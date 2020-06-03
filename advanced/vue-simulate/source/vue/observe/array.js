2579ab5804f27339130ce56895db25c9922dc331
wangkaiwd <1162486176@qq.com>
2020-06-04 00:04:13 +0800

    feat: add new object for array

--- advanced/vue-simulate/source/vue/observe/array.js
+++ advanced/vue-simulate/source/vue/observe/array.js
@@ -2,6 +2,8 @@
 // push shift unshift pop reverse sort splice
 
 // arrayMethods.__proto__ = Array.prototype
+import { observe } from './index';
+
 const ArrayPrototype = Array.prototype;
 export const arrayMethods = Object.create(Array.prototype);
 
@@ -15,11 +17,28 @@ const methods = [
   'splice'
 ];
 
+function observeArray (inserted) {
+  for (let i = 0; i < inserted; i++) {
+    observe(inserted[i]);
+  }
+}
+
 methods.forEach(method => {
   arrayMethods[method] = function (...args) {
     // 借用数组原型上的方法，用调用arrayMethods[method]方法的数组来调用Array.prototype上的方法
     const result = ArrayPrototype[method].call(this, ...args);
     console.log('调用改变原数组的方法');
+    let inserted = undefined;
+    switch (method) {
+      case 'push':
+      case 'unshift':
+        inserted = args;
+        break;
+      case 'splice': // splice
+        inserted = args.slice(2);
+        break;
+    }
+    observeArray(inserted);
     return result;
   };
 });
--- advanced/vue-simulate/src/index.js
+++ advanced/vue-simulate/src/index.js
@@ -21,7 +21,10 @@ const vm = new Vue({
 // Vue实例对象
 
 // 并不会触发Object.defineProperty中的set方法
-vm.arr.push(4);
+// vm.arr.push(4);
+
+// 如果数组中新增的内容是对象
+vm.arr.push({ a: 1 });
 console.log(vm);
 
 
