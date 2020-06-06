6544918f7bf32263e85ecdc1b79157fec7894fa7
wangkaiwd <1162486176@qq.com>
2020-06-07 01:11:31 +0800

    feat: save first text node content which contain curly brace

--- advanced/vue-simulate/source/vue/utils.js
+++ advanced/vue-simulate/source/vue/utils.js
@@ -14,6 +14,8 @@ export function getValue (vm, expr) {
 }
 
 export function compilerText (node, vm) {
+  // 为节点添加一个属性，在第一次渲染时该属性不存在，所以会保存第一次带有{{}}花括号的字符串，方便第二次即后几次更新时替换
+  // 这个属性在增加后也会一起添加到vm.$el中，所以下次移动到DocumentFragment中还是会存在
   if (!node.expr) {
     node.expr = node.textContent;
   }
