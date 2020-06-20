ce33bdbfa0a5918156bab83a5e582f7852160912
wangkaiwd <1162486176@qq.com>
2020-06-21 00:06:10 +0800

    feat: try understand vue.extend api

--- advanced/vue-usage/vue-components/src/main.js
+++ advanced/vue-usage/vue-components/src/main.js
@@ -3,7 +3,26 @@ import App from './App.vue';
 import './extend-prorotype';
 
 Vue.config.productionTip = false;
-
+console.log('App', App);
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+// 创建一个Vue实例
+// const vm = new Vue({
+//   name: 'OffDocument',
+//   render: h => h('div', 'hello')
+// }).$mount();
+//
+// document.body.appendChild(vm.$el);
+
+// 创建一个Vue的子类
+// const Ctor = Vue.extend({
+//   name: 'OffDocument',
+//   render: h => h('div', 'hello')
+// });
+//
+// const vm = new Ctor().$mount();
+// document.body.appendChild(vm.$el);
+
+// 感觉俩种方法都可以，在new的时候可以通过propsData传递属性值
