5efee295b835b7f1a27cf10ce88127498ef2869f
wangkaiwd <1162486176@qq.com>
2020-06-24 00:31:28 +0800

    feat: simple read document of vuex

--- advanced/vuex-usage/src/store/index.js
+++ advanced/vuex-usage/src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    age: 18
   },
   mutations: {
+    syncChange (state, payload) {
+      state.age += payload;
+    }
   },
   actions: {
+    asyncChange ({ commit }, payload) {
+
+    }
   },
-  modules: {
-  }
-})
+  modules: {}
+});
