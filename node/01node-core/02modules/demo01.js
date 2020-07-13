ea095d7d66abe896c554efdee5f3982ebbaa6f83
wangkaiwd <1162486176@qq.com>
2020-07-13 23:43:45 +0800

    feat: require method source code arrange

--- node/01node-core/02modules/demo01.js
+++ node/01node-core/02modules/demo01.js
@@ -17,8 +17,14 @@
 const a = require('./a');
 console.log(a);
 
-// 1. 执行require方法
-// 2. mod.require
-// 3. Module._load
-// 4. Module._resolveFilename
-// 5.
+// 执行步骤：
+// 1. Module._load 加载模块
+// 2. Module._resolveFilename 把相对路径转换成绝对路径
+// 3. const module = new Module 创建一个模块， 模块的信息： id, exports
+// 4. tryModuleLoad 尝试加载模块
+// 5. 通过不同的后缀加载 .json/.js
+// 6. Module._extensions 定义不同后缀文件的处理方法
+// 7. 核心是读取文件
+// 8. 外层文件增加一个函数，并且让函数执行(改变了this, export, module, require, __dirname, __filename)
+// 9. 用户给module.exports赋值
+// 10. 最终返回module.exports
