a0c7d116b4ca0ef478fa5f84d097e1da32f33b5e
wangkaiwd <1162486176@qq.com>
2020-08-09 16:43:24 +0800

    feat: create upload component file

--- advanced/vue-usage/vue-components/src/App.vue
+++ advanced/vue-usage/vue-components/src/App.vue
@@ -1,36 +1,19 @@
 <template>
   <div id="app">
-    <!--    <communicate></communicate>-->
-    <!--    <my-form></my-form>-->
-    <!--    <my-menu></my-menu>-->
-    <!--    <render-jsx></render-jsx>-->
-    <!--    <manually-mount></manually-mount>-->
-    <my-lazy></my-lazy>
+    <my-upload></my-upload>
   </div>
 </template>
 
 <script>
-  import communicate from '@/view/communicate';
-  import MyMenu from '@/view/my-menu';
-  import MyForm from '@/view/my-form';
-  import RenderJsx from '@/view/render-jsx';
-  import ManuallyMount from '@/view/manually-mount';
-  import MyLazy from '@/view/my-lazy';
+import MyUpload from './components/my-upload/my-upload';
 
-  export default {
-    name: 'App',
-    components: {
-      communicate,
-      MyMenu,
-      MyForm,
-      RenderJsx,
-      ManuallyMount,
-      MyLazy
-    },
-  };
+export default {
+  name: 'App',
+  components: { MyUpload },
+};
 </script>
 
 <style lang="scss">
-  #app {
-  }
+#app {
+}
 </style>
---
+++ advanced/vue-usage/vue-components/src/components/my-upload/my-upload.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-upload">
+    Upload
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyUpload',
+  data () {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.my-upload {
+
+}
+</style>
