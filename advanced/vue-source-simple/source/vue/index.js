2c009374146d949c5f85d615a902e488b5580f8e
wangkaiwd <1162486176@qq.com>
2020-08-26 01:10:23 +0800

    feat: why can't normal run webpack ? 🤔

---
+++ advanced/vue-source-simple/source/vue/index.js
@@ -0,0 +1,27 @@
+function Vue (options) {
+  const vm = this;
+  vm.$options = options;
+  initData(vm);
+}
+
+function defineProperty (target, key, value) {
+  Object.defineProperty(target, key, {
+    get: () => {
+      return value;
+    },
+    set: (val) => {
+      target[key] = val;
+    }
+  });
+}
+
+function initData (vm) {
+  let data = vm.$options.data;
+  data = typeof data === 'function' ? data() : data;
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      defineProperty(vm._data, key, data[key]);
+    }
+  }
+}
+
---
+++ advanced/vue-source-simple/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'eval-cheap-source-map',
+  output: { path: path.resolve(__dirname, 'dist') },
+  resolve: {
+    modules: ['./node_modules', './source'],
+    extensions: ['.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Vue Source',/* Load a custom template (lodash by default)*/
+      template: './index.html'
+    })
+  ],
+  devServer: { contentBase: './dist' },
+};
