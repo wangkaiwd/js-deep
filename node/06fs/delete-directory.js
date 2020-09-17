c7c249e4fefed338ba4f77e63e2a95b401320ba2
wangkaiwd <1162486176@qq.com>
2020-09-18 00:45:10 +0800

    feat: recursive delete all directories

--- node/06fs/delete-directory.js
+++ node/06fs/delete-directory.js
@@ -1,1 +1,22 @@
-// 删除目录
+const fs = require('fs');
+const path = require('path');
+
+// 删除目录: https://excalidraw.com/#json=5624225675083776,DPO3poRDUCBKt7_Sz6nkYw
+// 先删除子节点，然后再删除父节点
+function rmdirSync (p) {
+  const stat = fs.statSync(p);
+  if (stat.isDirectory()) {
+    const dirs = fs.readdirSync(p);
+    dirs.forEach(dir => {
+      const currentPath = path.resolve(p, dir);
+      rmdirSync(currentPath);
+    });
+    fs.rmdirSync(p);
+  } else {
+    fs.unlinkSync(p);
+  }
+}
+
+rmdirSync('a');
+
+
