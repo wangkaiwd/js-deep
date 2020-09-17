c39bb0d5d319b21441ffc7bbd9a28ed928d1564a
wangkaiwd <1162486176@qq.com>
2020-09-18 01:05:00 +0800

    feat: supply some annotation

--- node/06fs/delete-directory.js
+++ node/06fs/delete-directory.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 // 删除目录: https://excalidraw.com/#json=5624225675083776,DPO3poRDUCBKt7_Sz6nkYw
 // 先删除子节点，然后再删除父节点
+// 深度遍历删除
+// 'a/b'
+// 有一点想不太明白
 function rmdirSync (p) {
   const stat = fs.statSync(p);
   if (stat.isDirectory()) {
@@ -18,5 +21,3 @@ function rmdirSync (p) {
 }
 
 rmdirSync('a');
-
-
