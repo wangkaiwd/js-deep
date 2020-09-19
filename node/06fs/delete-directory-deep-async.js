aa90772f7da79087723644a9a9e8916d0ef31798
wangkaiwd <1162486176@qq.com>
2020-09-19 23:50:35 +0800

    feat: implement async delete but not understand why

---
+++ node/06fs/delete-directory-deep-async.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+
+// a a/b a/b/c
+// so difficult to think
+function rmdirAsync (p, cb) {
+  fs.stat(p, (err, stats) => {
+    if (stats.isDirectory()) {
+      fs.readdir(p, (err, files) => {
+        const fullFiles = files.map(file => path.resolve(p, file));
+        let index = 0;
+
+        function next () {
+          if (index === fullFiles.length) {
+            return fs.rmdir(p, cb);
+          }
+          const fullFile = fullFiles[index++];
+          rmdirAsync(fullFile, next);
+        }
+
+        next();
+      });
+    } else {
+      fs.unlink(p, (err) => {
+        if (err) {
+          console.log('err', err);
+        }
+        cb();
+      });
+    }
+  });
+}
+
+rmdirAsync('a', () => {
+  console.log('delete success');
+});
