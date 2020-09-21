dbccefbf6086f8b4a4d7fb98604517899a0699a6
wangkaiwd <1162486176@qq.com>
2020-09-22 00:45:19 +0800

    feat: create stream directory and add some note

---
+++ node/07stream/demo01.js
@@ -0,0 +1,15 @@
+// fs.open fs.read fs.write fs.close
+const fs = require('fs');
+const path = require('path');
+
+// return a new initialized Buffer of the specified size.
+const buf = Buffer.alloc(3);
+
+// mode: 0o666
+// 权限：读取权限  写入权限  执行权限
+// rwx(表示用户是否可读可写可执行) rwx(我所属组是否有读写执行的权限) rwx
+// fd: 文件描述符，它代表了文件name.txt拥有了读取这个文件的权限，类型是一个number类型
+// https://stackoverflow.com/a/36771339/11720536
+fs.open(path.resolve(__dirname, 'name.txt'), (err, fd) => {
+  console.log('open1', fd);
+});
---
+++ node/07stream/readme.md
@@ -0,0 +1,8 @@
+## Stream
+* fs.copy
+
+* fs.open
+* fs.read
+* fs.write
+* fs.close
+
