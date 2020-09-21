c4df09c6ca3588c4b9bab0ddac9cb52ec541fe75
wangkaiwd <1162486176@qq.com>
2020-09-22 01:08:25 +0800

    feat: use fs.write read content from buffer

--- node/07stream/demo01.js
+++ node/07stream/demo01.js
@@ -3,16 +3,27 @@ const fs = require('fs');
 const path = require('path');
 
 // return a new initialized Buffer of the specified size.
-const buf = Buffer.alloc(3);
+// const buf = Buffer.alloc(3);
 
+// 读相关
 // mode: 0o666
 // 权限：读取权限  写入权限  执行权限
 // rwx(表示用户是否可读可写可执行) rwx(我所属组是否有读写执行的权限) rwx
 // fd: 文件描述符，它代表了文件name.txt拥有了读取这个文件的权限，类型是一个number类型
 // https://stackoverflow.com/a/36771339/11720536
-fs.open(path.resolve(__dirname, 'name.txt'), (err, fd) => {
-  fs.read(fd, buf, 0, 3, 0, (err, bytesRead) => {
-    console.log('bytesRead', bytesRead);
-    console.log(buf.toString());
+// fs.open(path.resolve(__dirname, 'name.txt'), (err, fd) => {
+//   fs.read(fd, buf, 0, 3, 0, (err, bytesRead) => {
+//     console.log('bytesRead', bytesRead);
+//     console.log(buf.toString());
+//     fs.close(fd, () => {});
+//   });
+// });
+
+// 写相关
+const buf = Buffer.from('张三李四');
+// 注意：flags默认为r,所以在这里必须要指定
+fs.open(path.resolve(__dirname, 'name.txt'), 'w', (err, fd) => {
+  fs.write(fd, buf, 9, 3, 0, (err, written, buffer) => {
+    console.log('written', written);
   });
 });
