ebcdaafbcd0279a518b9780ce94033aeccfd3d59
wangkaiwd <1162486176@qq.com>
2020-09-23 21:54:03 +0800

    feat: preliminary implement readable stream

--- node/07stream/ReadableStream.js
+++ node/07stream/ReadableStream.js
@@ -1,11 +1,61 @@
 const EventEmitter = require('events');
+const fs = require('fs');
 
 // 继承EventEmitter
 class ReadableStream extends EventEmitter {
-  constructor () {
+  constructor (path, options = {}) {
     super();
+    this.path = path;
+    this.flags = options.flags || 'r';
+    this.encoding = options.encoding;
+    this.fd = options.fd;
+    this.mode = options.mode || 0O666;
+    this.autoClose = options.autoClose || true;
+    this.start = options.start || 0;
+    this.end = options.end;
+    this.highWaterMark = options.highWaterMark || 64 * 1024;
+    this.pos = 0;
+    this.open();
+    this.on('newListener', (type) => {
+      // 监听'data'事件流是暂停模式的，监听data事件后会自动变成流动模式
+      if (type === 'data') {
+        this.read();
+      }
+    });
   }
 
+  open () {
+    fs.open(this.path, this.flags, this.mode, (err, fd) => {
+      if (err) {
+        this.emit('error', err);
+      }
+      this.fd = fd;
+      this.emit('open', fd);
+    });
+  }
+
+  // 这里会同步执行，而打开文件是异步任务
+  read () {
+    if (typeof this.fd !== 'number') {
+      return this.once('open', this.read);
+    }
+    const buffer = Buffer.alloc(this.highWaterMark);
+    fs.read(this.fd, buffer, 0, this.highWaterMark, this.pos, (err, bytesRead) => {
+      if (!bytesRead) {
+        this.emit('end');
+        return this.close();
+      }
+      this.pos += bytesRead;
+      this.emit('data', buffer);
+      this.read();
+    });
+  }
+
+  close () {
+    fs.close(this.fd, () => {
+      this.emit('close');
+    });
+  }
 }
 
 module.exports = ReadableStream;
--- node/07stream/demo03.js
+++ node/07stream/demo03.js
@@ -4,7 +4,9 @@
 
 // 文件流：特有，打开和关闭方法
 const fs = require('fs');
-const rs = fs.createReadStream('./name.txt', {
+const ReadableStream = require('./ReadableStream');
+// const rs = fs.createReadStream('./name.txt', {
+const rs = new ReadableStream('./name.txt', {
   flags: 'r', // 文件系统标识
   encoding: null, // 编码， 默认是buffer
   fd: null, // 文件描述符，表示打开的文件
@@ -12,7 +14,7 @@ const rs = fs.createReadStream('./name.txt', {
   autoClose: true, // 是否自动关闭
   start: 0, // start 和 end 都会包含，并且start从0开始计数
   end: 3,
-  highWaterMark: 3, // 表示每次读几个
+  highWaterMark: 2, // 表示每次读几个
 });
 
 rs.on('open', (fd) => {
@@ -24,7 +26,7 @@ const arr = [];
 // 默认可读流是暂停模式的，
 // 当监听data事件后会自动变成流动模式
 rs.on('data', (data) => {
-  console.log('data', data);
+  console.log('data', data, data.toString());
   str += data; // 当是文字的时候会读取出错
   arr.push(data);
 });
@@ -33,7 +35,9 @@ rs.on('end', () => {
   console.log('end', str);
   console.log('end1', Buffer.concat(arr).toString());
 });
-
+rs.on('error', (err) => {
+  console.log('error', err);
+});
 // 文件读取完毕才会触发
 rs.on('close', (fd) => {
   console.log('文件关闭了');
