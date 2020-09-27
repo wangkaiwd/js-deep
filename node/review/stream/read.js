134fb037611874d100e23f4693582806e3256915
wangkaiwd <1162486176@qq.com>
2020-09-27 15:37:28 +0800

    feat: usage of readable stream

---
+++ node/review/stream/read.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const absPath = (...dir) => path.resolve(__dirname, ...dir);
+
+const rs = fs.createReadStream(absPath('name.txt'), {
+  flags: 'r', // 文件系统标识符
+  encoding: null, // 默认为buffer
+  mode: 0o666, // 设置文件权限和粘滞位(permission and sticky bits)的模式, 但是只会在文件在被创建之后
+  fd: null,
+  autoClose: true,
+  start: 0, // 写入文件的字节范围(read a range of bytes from file instead of entire file)
+  end: Infinity,
+  highWaterMark: 3, // 默认 64kb => 64 * 1024
+});
+
+// 文件流会有open和close事件
+rs.on('open', (fd) => {
+  console.log('open', fd);
+});
+
+const arr = [];
+// not specify default encoding, data will be buffer
+rs.on('data', (data) => {
+  // push return new length of the array
+  console.log('data', arr.push(data));
+});
+
+rs.on('end', () => {
+  console.log('end', Buffer.concat(arr).toString());
+});
+rs.on('error', (err) => {
+  console.log('err', err);
+});
+
+rs.on('close', () => {
+  console.log('close');
+});
