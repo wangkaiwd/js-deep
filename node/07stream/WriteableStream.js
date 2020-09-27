519dead9a2f3b242230330ad3c1ee5575bf11646
wangkaiwd <1162486176@qq.com>
2020-09-27 21:45:51 +0800

    feat: emit writable stream open event

--- node/07stream/WriteableStream.js
+++ node/07stream/WriteableStream.js
@@ -3,8 +3,36 @@ const fs = require('fs');
 const path = require('path');
 
 class WriteableStream extends EventEmitter {
-  constructor () {
+  constructor (path, options) {
     super();
+    this.initProperty(path, options);
+    this.open();
   }
 
+  initProperty (path, options) {
+    this.path = path;
+    this.fd = options.fd || null;
+    this.mode = options.mode || 0o666;
+    this.flags = options.flags || 'w';
+    this.autoClose = options.autoClose || true;
+    this.encoding = options.encoding || 'utf8';
+    this.start = options.start || 0;
+    this.highWaterMark = options.highWaterMark || 16 * 1024;
+  }
+
+  open () {
+    fs.open(this.path, this.flags, this.mode, (err, fd) => {
+      if (err) {
+        return this.emit('error', err);
+      }
+      this.fd = fd;
+      this.emit('open', fd);
+    });
+  }
+
+  write () {
+
+  }
 }
+
+module.exports = WriteableStream;
--- node/07stream/write.js
+++ node/07stream/write.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-
-const ws = fs.createWriteStream(path.resolve(__dirname, 'name2.txt'), {
+const WritableStream = require('./WriteableStream');
+// const ws = fs.createWriteStream(path.resolve(__dirname, 'name2.txt'), {
+const ws = new WritableStream(path.resolve(__dirname, 'name2.txt'), {
   fd: null,
   mode: 0o666,
   flags: 'w',
@@ -10,7 +11,9 @@ const ws = fs.createWriteStream(path.resolve(__dirname, 'name2.txt'), {
   start: 0,
   highWaterMark: 3
 });
-
+ws.on('open', (fd) => {
+  console.log('open', fd);
+});
 // For streams not operating in object mode, chunk must be a string, Buffer or Unit8Array.
 // For object mode streams, chunk may any JavaScript value other than null
 ws.write('1', () => {
