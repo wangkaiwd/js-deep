3b4921ae4780a4a5d5b35605cb374017eccb5838
wangkaiwd <1162486176@qq.com>
2020-09-28 11:50:22 +0800

    feat: create linked list basic structure

---
+++ node/review/stream/simpleLinkedList.js
@@ -0,0 +1,35 @@
+class Node {
+  constructor (element, next) {
+    this.element = element;
+    this.next = next;
+  }
+}
+
+class SimpleLinkedList {
+  constructor () {
+    this.head = null;
+    this.size = 0;
+  }
+
+  add () {
+
+  }
+
+  get () {
+
+  }
+
+  set () {
+
+  }
+
+  remove () {
+
+  }
+
+  clear () {
+
+  }
+}
+
+module.exports = SimpleLinkedList;
