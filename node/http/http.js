84a118f6c70f3c46c27adb0bff19039e703674b9
wangkaiwd <1162486176@qq.com>
2020-09-29 22:38:53 +0800

    feat: implement port accumulate

--- node/http/http.js
+++ node/http/http.js
@@ -1,2 +1,42 @@
 const http = require('http');
 
+// 单线程：这里会发生阻塞
+const blockClient = (req, res) => {
+  if (req.url === '/sum') {
+    let sum = 0;
+    for (let i = 0; i < 10000000000; i++) {
+      sum += i;
+    }
+    res.end(sum + '');
+  } else {
+    res.end('ok');
+  }
+};
+
+// http.createServer([options][,requestListener])
+// requestListener 是一个被自动添加到`request`事件的事件
+
+// 下面的俩个请求监听函数都会触发
+
+const server = http.createServer((req, res) => {
+  console.log('create server');
+});
+
+// server.on('request', (req, res) => {
+//   console.log('on request');
+// });
+
+// 80,443
+// 端口尽量不要低于3000,容易被系统占用
+let port = 3000;
+server.listen(port, () => {
+  console.log(`server start on port ${port}`);
+});
+server.on('error', (err) => {
+  // console.log('err', err);
+  if (err.code === 'EADDRINUSE') {
+    // 这里不用再次监听回调，当触发listen事件的时候会触发第一次监听时传入的函数
+    console.log('err emit');
+    server.listen(++port);
+  }
+});
--- node/http/usageOfHttp.md
+++ node/http/usageOfHttp.md
@@ -1,5 +1,11 @@
 ### HTTP
+* server = [http.createServer](https://devdocs.io/node~10_lts/http#http_http_createserver_options_requestlistener)
+  * 传入的`requestListener`会自动添加到`request`事件
+  * server.on('request',(req,res) => {})(前一条相当于自动监听request方法)
 * js 是单线程的， node.js也是单线程
-* 监听方法尽量使用异步，否则会阻塞响应
+  * 监听方法尽量使用异步，否则会阻塞响应
 * 端口被占用，重开端口再次监听
 * 中间层
+
+
+ 
