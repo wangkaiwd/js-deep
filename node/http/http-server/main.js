92c0d9c54d37868c445d717ced5608e6fdb132fe
wangkaiwd <1162486176@qq.com>
2020-10-03 17:47:24 +0800

    feat: use ETag to cache http response

--- node/http/http-server/main.js
+++ node/http/http-server/main.js
@@ -3,6 +3,7 @@ const mime = require('mime');
 const path = require('path');
 const url = require('url');
 const { promisify } = require('util');
+const crypto = require('crypto');
 const fs = require('fs').promises;
 const ejs = require('ejs');
 const renderFile = promisify(ejs.renderFile);
@@ -41,42 +42,58 @@ class Server {
   //    expires: 过期时间
   //    cache-control
   // 2. 协商缓存：缓存期间，文件内容发生了变化
-  cache (req, res, stat) {
+  cache (req, res, absPath, stat) {
     // 设置10s的缓存，为了保险起见，Expires和cache-control都会被设置
     // 同时设置cache-control max-age与Expires时会忽略Expires
-    res.setHeader('Expires', new Date(Date.now() + 15 * 1000));
+    // res.setHeader('Expires', new Date(Date.now() + 15 * 1000));
     // res.setHeader('cache-control', 'max-age=15');
     // 每次都会请求服务器，询问文件是否发生了变化
     // 根据文件更改时间来判断文件是否发生更改
     // res.setHeader('cache-control', 'no-cache');
     // 协商缓存和强制缓存一起设置，10s内从缓存中查找，10s后服务端进行修改时间比对
     // 之后继续重复缓存步骤
-    res.setHeader('cache-control', 'max-age=10');
+    // res.setHeader('cache-control', 'max-age=10');
+    return fs.readFile(absPath).then((data) => {
+      res.setHeader('Expires', new Date(Date.now() + 15 * 1000));
+      res.setHeader('cache-control', 'max-age=10');
+      const etag = crypto.createHash('md5').update(data).digest('base64');
+      const ifNoneMatch = req.headers['if-none-match'];
+      res.setHeader('ETag', etag);
+      return etag === ifNoneMatch;
+    }).then((cacheable) => {
+      if (cacheable) {return cacheable;}
+      const ctime = stat.ctime.toString(); // 文件最后一次修改时间
+      const ifModifiedSince = req.headers['if-modified-since'];
+      res.setHeader('Last-Modified', ctime);
+      return Promise.resolve(ctime === ifModifiedSince);
+    });
     // Last-Modified
     // 要注意这里的事件要统一格式
-    const ctime = stat.ctime.toString(); // 文件最后一次修改时间
+    // const ctime = stat.ctime.toString(); // 文件最后一次修改时间
     // 上一次的修改时间
     // headers中 key是为小写
-    const ifModifiedSince = req.headers['if-modified-since'];
-    res.setHeader('Last-Modified', ctime);
+    // const ifModifiedSince = req.headers['if-modified-since'];
+    // res.setHeader('Last-Modified', ctime);
     // 最后一次时间相同，说明使用缓存，否则要重新请求资源
-    return ctime === ifModifiedSince;
+    // return ctime === ifModifiedSince;
   }
 
   readFile (req, res, absPath, stat) {
     return new Promise((resolve, reject) => {
       const fileType = mime.getType(absPath);
       res.setHeader('Content-Type', `${fileType};charset=utf-8`);
-      if (this.cache(req, res, stat)) {
-        res.statusCode = 304;
-        res.end();
-        return resolve();
-      }
-      res.statusCode = 200;
-      // 注意，这个过程是异步的,通过Promise处理成同步逻辑
-      createReadStream(absPath).pipe(res)
-        .on('finish', resolve)
-        .on('error', (e) => reject(e));
+      return this.cache(req, res, absPath, stat).then((cacheable) => {
+        if (cacheable) {
+          res.statusCode = 304;
+          res.end();
+          return resolve();
+        }
+        res.statusCode = 200;
+        // 注意，这个过程是异步的,通过Promise处理成同步逻辑
+        createReadStream(absPath).pipe(res)
+          .on('finish', resolve)
+          .on('error', (e) => reject(e));
+      });
     });
   }
 
