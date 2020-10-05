cf7c7b6a7b3ccc465c2d29eb89a90d62840b28f7
wangkaiwd <1162486176@qq.com>
2020-10-06 01:15:22 +0800

    feat: proxy request property for context object

--- node/kao/koa/lib/context.js
+++ node/kao/koa/lib/context.js
@@ -1,7 +1,13 @@
-const context = {
-  get url () {
-    return this.request.url;
-  }
-};
+const context = {};
 
+// __defineGetter__方法已经被废弃
+function defineGetter (target, key) {
+  context.__defineGetter__(key, function () {
+    return this[target][key];
+  });
+}
+
+// 感觉这样写也不太好，可以使用类似于vue将this._data中的属性代理到this上的方法来实现
+defineGetter('request', 'url');
+defineGetter('request', 'path');
 module.exports = context;
--- node/kao/koa/lib/request.js
+++ node/kao/koa/lib/request.js
@@ -1,6 +1,10 @@
+const url = require('url');
 const request = {
   get url () {
     return this.req.url;
+  },
+  get path () {
+    return url.parse(this.req.url).pathname;
   }
 };
 
