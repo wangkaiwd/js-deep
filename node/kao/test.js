034c983c5a6ba12f7edc7a8960e193b68715520c
wangkaiwd <1162486176@qq.com>
2020-10-09 00:52:06 +0800

    feat: implement template engine

--- node/kao/test.js
+++ node/kao/test.js
@@ -3,16 +3,21 @@ const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
 const content = fs.readFileSync(path.join(__dirname, 'test.html'), 'utf8');
-console.log(content);
 
 const render = (str, data) => {
+  const head = 'let html = ""\r\nwith(data) {\r\nhtml+=`';
   // .+:一个或多个任意字符
   // (...): 捕获parenthesis内的内容
-  str = str.replace(/<%=(.+)%>/g, (...args) => {
-    // 匹配到的字符串
-    return args[1];
+  str = str.replace(/<%=(.+?)%>/g, (...args) => { // 只会替换匹配到的内容，并且最终返回源字符串被替换后的结果
+    return '${' + args[1] + '}';
   });
+  const content = str.replace(/<%(.+?)%>/g, (...args) => {
+    // 匹配到的字符串
+    return '`\r\n' + args[1] + '\r\nhtml+=`';
+  });
+  const tail = '`}\r\nreturn html';
+  return new Function('data', head + content + tail)(data);
 };
-render(content);
+console.log(render(content, { arr: [1, 2, 3], name: '张三', age: 18 }));
 // const html = ejs.render(content, { name: '张三', age: 18 });
 // console.log('html', html);
