e9863afe5688c642c1c5dbb2eacd0d753ede9be0
wangkaiwd <1162486176@qq.com>
2020-10-10 00:02:28 +0800

    feat: try domain attribute

--- node/kao/cookie-session/cookie.js
+++ node/kao/cookie-session/cookie.js
@@ -1,6 +1,6 @@
 const http = require('http');
 // key,value
-// domain
+// domain: 指定那个主机允许接收cookie。默认为设置cookie的同源域名，不包括子域名
 // path: 为了发送Cookie头，path必须在请求URL中存在
 // expires/max-age： 生存时间
 // httpOnly：一个具有`HttpOnly`属性的`cookie`对于`JavaScript`的`Document.cookie`来说是无法访问的，只能被服务器发送。
@@ -9,12 +9,14 @@ const server = http.createServer((req, res) => {
     // Use an array of string here to send multiple headers with the same name
     // 这里使用字符串数组来发送拥有相同名字的多个响应头
     // 不能使用中文？
-    res.setHeader('Set-Cookie', ['name=zs', 'age=10']);
+    // 设置域名后，只有在指定域名及其子域名可以访问
+    res.setHeader('Set-Cookie', ['name=zs; Domain=.b.cn', 'age=10; httpOnly=true']);
     res.end('write ok');
+  } else if (req.url === '/read') {
+    res.end(req.headers.cookie || 'empty');
   } else {
     res.end('Not Found');
   }
-
 });
 server.listen(3000, () => {
   console.log('server is listening on port 3000');
