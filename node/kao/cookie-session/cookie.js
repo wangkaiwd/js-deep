6cf6346da39c8d64f859dd163b8779d54c9789eb
wangkaiwd <1162486176@qq.com>
2020-10-10 00:27:47 +0800

    feat: set lifetime of cookie

--- node/kao/cookie-session/cookie.js
+++ node/kao/cookie-session/cookie.js
@@ -2,7 +2,7 @@ const http = require('http');
 // key,value
 // domain: 指定那个主机允许接收cookie。默认为设置cookie的同源域名，不包括子域名
 // path: 为了发送Cookie头，path必须在请求URL中存在(以path开头的路径，默认为/，匹配所有请求路径)
-// expires/max-age： 生存时间
+// expires(绝对时间)/max-age(相对时间)： 生存时间 1. Session, 2. Expires/Max-Age
 // httpOnly：一个具有`HttpOnly`属性的`cookie`对于`JavaScript`的`Document.cookie`来说是无法访问的，只能被服务器发送。
 const server = http.createServer((req, res) => {
   if (req.url === '/write') {
@@ -10,7 +10,15 @@ const server = http.createServer((req, res) => {
     // 这里使用字符串数组来发送拥有相同名字的多个响应头
     // 不能使用中文？
     // 设置域名后，只有在指定域名及其子域名可以访问
-    res.setHeader('Set-Cookie', ['name=zs; Domain=.b.cn', 'age=10; httpOnly=true', 'test=hhh; path=/write']);
+    res.setHeader('Set-Cookie', [
+        'name=zs; Domain=.b.cn',
+        'age=10; httpOnly=true',
+        'test=hhh; path=/write',
+        'max-age-time=10s; max-age=10',
+        // 设置时间好像没有成功
+        `expires-time=10s; expires=${new Date(Date.now() + 10 * 1000).toString()}`
+      ]
+    );
     res.end('write ok');
   } else if (req.url === '/read') {
     res.end(req.headers.cookie || 'empty');
