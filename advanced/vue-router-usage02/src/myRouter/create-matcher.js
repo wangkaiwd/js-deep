a95841212d4763c7b9b016318edaa7f4b9530ccb
wangkaiwd <1162486176@qq.com>
2020-10-18 00:21:53 +0800

    feat: create map relation between route record and current url hash

--- advanced/vue-router-usage02/src/myRouter/create-matcher.js
+++ advanced/vue-router-usage02/src/myRouter/create-matcher.js
@@ -2,17 +2,18 @@ import createRouteMap from './create-route-map';
 
 const createMatcher = (routes) => {
   const { pathList, pathMap } = createRouteMap(routes);
+  console.log('info', pathList, pathMap);
   // 根据路径匹配对应的路由记录
   const match = (path) => {
-
   };
   // 用于动态添加路由
-  const addRoute = (routes) => {
-
+  const addRoutes = (routes) => {
+    // 在原来的基础上在新增路径列表以及路由和路径的映射关系
+    createRouteMap(routes, pathList, pathMap);
   };
   return {
     match,
-    addRoute
+    addRoutes
   };
 };
 export default createMatcher;
