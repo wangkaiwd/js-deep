c9c553f5f0b03cf2e28c528b54947db0fa11adc7
wangkaiwd <1162486176@qq.com>
2020-10-19 00:03:30 +0800

    feat: try Vue.observable and Vue.$set api

--- advanced/vue-router-usage02/src/myRouter/install.js
+++ advanced/vue-router-usage02/src/myRouter/install.js
@@ -11,6 +11,7 @@ const install = function (Vue) {
         // 创建一个响应式对象。内部`Vue`在通过data函数返回的对象上使用这个方法。
         // 注意这个方法创建的响应式数据在重新赋值后，就不具有响应式了，可能是没有设置get,set方法？导致set时候对其进行了重新赋值，而不是触发this._data中对应属性的set方法
         // this._route = Vue.observable(this._router.history.current);
+        // 这里不能使用Vue.$set方法，它只能为Vue的响应式属性添加一个新的响应式属性，目标对象不能是Vue实例，或者根data对象。即不能为this添加响应式属性，只能为this中的某个属性添加
       } else {
         this._routerRoot = this.$parent && this.$parent._routerRoot;
         // 实例中获取router实例： this._routerRoot._router
