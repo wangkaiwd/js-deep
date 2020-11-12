cbf2d163f8fdecd5760d3382f1746ee8b66113c3
wangkaiwd <1162486176@qq.com>
2020-11-13 00:39:40 +0800

    feat: think vue lifecycle flow chart

--- vue3/src/init.js
+++ vue3/src/init.js
@@ -3,8 +3,22 @@ import initState from './state';
 function initMixin (Vue) {
   Vue.prototype._init = function (options) {
     this.$options = options;
+    const vm = this;
+    let { el, template } = this.$options;
     // 扩展不同的初始化功能
     initState(this);
+    if (el) { // 没有el选项，会调用$mount方法，进行手动挂载
+      // 是否有template,如果有template,将template 转换为render 函数
+      // 如果没有，将el.outerHTML作为template
+      if (!template) {
+        template = el.outerHTML;
+      }
+      // 开始解析HTML
+    }
+    // 创建vm.$el来替代el
+  };
+  Vue.prototype.$mount = function (el) {
+
   };
 }
 
