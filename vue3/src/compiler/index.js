38f3597fd613138040b059e9aa34f94c5114f3c8
wangkaiwd <1162486176@qq.com>
2020-11-14 19:38:27 +0800

    feat: parse html attributes between start tag and end tag

--- vue3/src/compiler/index.js
+++ vue3/src/compiler/index.js
@@ -19,15 +19,65 @@
 // };
 
 const ncname = `[a-zA-Z_][\\-\\.0-9_a-zA-Z]*`; // 标签名 <aa></aa>
-const qnameCapture = `((?:${ncname}\\:)?${ncname})`; // <my:xxx></my:xxx>
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`; // <my:xxx></my:xxx>， 捕获内容为标签名
 const startTagOpen = new RegExp(`^<${qnameCapture}`);
 const startTagClose = /^\s*(\/?)>/;
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`); // 匹配结尾标签
 const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
-const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // 匹配双花括号之间除了换行之外的任意字符
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; // 匹配双花括号之间的任意字符
+
+// 开始标签
+function start (tagName, attrs) {
+
+}
+
+// 结束标签
+function end () {
+
+}
+
+// 文本
+function chars () {
+
+}
 
 function parseHTML (html) {
-  return undefined;
+  while (html) { // 一点点删除html
+    const textEnd = html.indexOf('<'); // 标签会以'<'开头
+    if (textEnd === 0) {
+      const startTagMatch = parseStartTag();
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attrs);
+      }
+    }
+    break;
+  }
+
+  function parseStartTag () {
+    const start = html.match(startTagOpen); // 没有匹配到会返回null
+    if (start) {
+      const match = { tagName: start[1], attrs: [] };
+      advance(start[0].length);
+      // 递归匹配属性
+      let end = html.match(endTag);
+      let attr = html.match(attribute);
+      while (!end && attr) {
+        match.attrs.push({ name: attr[1], value: attr[3] || attr[4] || attr[5] });
+        advance(attr[0].length);
+        end = html.match(endTag);
+        attr = html.match(attribute);
+      }
+      if (end) {
+        advance(end[0].length);
+        return match;
+      }
+    }
+  }
+
+  // 删除已经处理过的内容
+  function advance (startIndex) {
+    return html = html.substring(startIndex);
+  }
 }
 
 // 将html编译为函数
