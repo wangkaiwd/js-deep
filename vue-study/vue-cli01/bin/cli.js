ef2eeca84ddd6cd6aa661326159414711b178fc6
wangkaiwd <1162486176@qq.com>
2020-12-05 23:07:58 +0800

    feat: add create command for cli

--- vue-study/vue-cli01/bin/cli.js
+++ vue-study/vue-cli01/bin/cli.js
@@ -1,2 +1,31 @@
 #!/usr/bin/env node
-console.log('hello cli');
+const commander = require('commander');
+const program = new commander.Command();
+const pkg = require('../package.json');
+/**
+ * 将命令后传入的选项处理为对象
+ * @param cmd
+ * @returns {*}
+ */
+const getOptions = (cmd) => {
+  return cmd.options.reduce((options, cur) => {
+    const key = cur.long.slice(2);
+    if (cmd[key]) {
+      options[key] = cmd[key];
+    }
+    return options;
+  }, {});
+};
+program
+  .name('my-cli')
+  .usage('<command> [options]')
+  .version(pkg.version);
+
+program
+  .command('create <app-name>')
+  .description('create a new vue project')
+  .option('-f,--force', 'overwrite target directory if it exists')
+  .action((name, cmd) => {
+    const options = getOptions(cmd);
+  });
+program.parse(process.argv);
