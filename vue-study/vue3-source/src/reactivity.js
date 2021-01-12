66338a1ea0135b7413e17ba5b55e6e63c3a5d058
wangkaiwd <1162486176@qq.com>
2021-01-13 01:14:13 +0800

    feat: combine reflect with proxy

--- vue-study/vue3-source/src/reactivity.js
+++ vue-study/vue3-source/src/reactivity.js
@@ -2,6 +2,7 @@ let activeEffect;
 
 export function effect (fn) {
   activeEffect = fn;
+  fn();
 }
 
 // Proxy get: https://javascript.info/proxy#default-value-with-get-trap
@@ -12,9 +13,14 @@ export function reactive (target) {
       // const result = Reflect.set(target, key, value);
       // 设置值时，可以执行页面更新的操作
       // return result;
-      target[key] = value;
+      // target[key] = value;
+      // The set trap should return true if setting is successful, and false otherwise
+      // Reflect.set有对应的返回值
+      // 对于每一个被Proxy可以捕获的内部方法，在Reflect中都有相应的方法，拥有和Proxy捕获器相同的名字和参数
+      const result = Reflect.set(target, key, value);
       // 设置值时更新视图
       activeEffect?.();
+      return result;
     },
     // get () {
     //
