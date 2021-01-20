e85fd25076add3f2e08417c1eef88c414590c83f
wangkaiwd <1162486176@qq.com>
2021-01-20 16:48:56 +0800

    feat: implement vuex simplest demo

---
+++ advanced/vuex-source/vuex-usage02/README.md
@@ -0,0 +1,39 @@
+## handwrite simple vuex
+
+write vuex step by step
+
+### Problem
+
+* why actions only store async logic
+
+### What is vuex
+
+Vuex is a global object in Vue application. Different from normal object of JavaScript, data in it is reactive.
+
+Do a simple demo with vuex.
+
+### Feature
+
+will implement following features:
+
+```javascript
+const store = new Vuex.store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
+})
+```
+
+* state
+* getters
+* mutations
+* actions
+* modules
+* namespace
+* dynamic register module
+* plugin
+  * implement logger official plugin
+* replaceState
+* helper
---
+++ advanced/vuex-source/vuex-usage02/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
+    <h3>a:{{ a }}</h3>
+    <h3>c:{{ c }}</h3>
+    <button @click="addA">addA</button>
+    <button @click="addC">addC</button>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {};
+  },
+  mounted () {
+    // 将Vue的实例放到了$store属性上
+    console.log(this.$store);
+  },
+  computed: {
+    a () {
+      return this.$store.state.a;
+    },
+    c () {
+      return this.$store.state.b.c;
+    }
+  },
+  methods: {
+    addA () {
+      this.$store.commit('addA');
+    },
+    addC () {
+      this.$store.commit('addC');
+    }
+  }
+};
+</script>
