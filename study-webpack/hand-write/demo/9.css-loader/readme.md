c3470b5078f25c4648e88685f5cdda46d5789743
wangkaiwd <1162486176@qq.com>
2021-02-04 17:39:44 +0800

    feat: know css loader build progress

--- study-webpack/hand-write/demo/9.css-loader/readme.md
+++ study-webpack/hand-write/demo/9.css-loader/readme.md
@@ -1,2 +1,11 @@
 ## css-loader
+
 * to string loader
+* analyse css-loader build file
+  * 4 step
+  * handle @import syntax(list)
+  * [this.addDependency](https://webpack.js.org/api/loaders/#thisadddependency)
+
+postcss:
+
+* postcss ast 
--- study-webpack/hand-write/demo/9.css-loader/webpack.config.js
+++ study-webpack/hand-write/demo/9.css-loader/webpack.config.js
@@ -3,11 +3,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
+  devtool: 'source-map',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist')
   },
-  module: {},
+  resolveLoader: {
+    modules: [path.resolve(__dirname, 'loaders'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'to-string-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              esModule: false,
+              import: false
+            }
+          }
+        ]
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin(),
     new CleanWebpackPlugin()
